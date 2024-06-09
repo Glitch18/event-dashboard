990b225cb2001812b4eb897271a470de2c6490cb
Atharva <atharvakn@gmail.com>
2024-06-09 14:13:53 +0530

    refac: formatting pass

--- src/eventListener.ts
+++ src/eventListener.ts
@@ -1,19 +1,19 @@
-import { Contract, ContractEventPayload, ethers } from "ethers";
+import { Contract, ContractEventPayload, ethers } from 'ethers'
 
 export type UserOperationEvent = {
-  userOpHash: string;
-  sender: string;
-  paymaster: string;
-  nonce: number;
-  success: boolean;
-  actualGas: number;
-  gasUsed: number;
-  payload: ContractEventPayload;
-};
+  userOpHash: string
+  sender: string
+  paymaster: string
+  nonce: number
+  success: boolean
+  actualGas: number
+  gasUsed: number
+  payload: ContractEventPayload
+}
 
 // TODO: Try replacing with https://medium.com/coinmonks/top-10-polygon-apis-6187fc965851
 export const listen = async (contract: Contract, eventName: string) => {
-  console.log(`Listening for ${eventName} events...`);
+  console.log(`Listening for ${eventName} events...`)
   contract.on(eventName, (...args) => {
     // LOGGER.info(`Received event: ${eventName}`);
     const userOpEvent: UserOperationEvent = {
@@ -25,7 +25,7 @@ export const listen = async (contract: Contract, eventName: string) => {
       actualGas: args[5],
       gasUsed: args[6],
       payload: args[7],
-    };
+    }
     // Send to database
-  });
-};
+  })
+}
