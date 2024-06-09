b7c00194d84fdc87aa637b0101ba7df804d4d004
Atharva <atharvakn@gmail.com>
2024-06-09 14:12:34 +0530

    initial commit

---
+++ src/eventListener.ts
@@ -0,0 +1,31 @@
+import { Contract, ContractEventPayload, ethers } from "ethers";
+
+export type UserOperationEvent = {
+  userOpHash: string;
+  sender: string;
+  paymaster: string;
+  nonce: number;
+  success: boolean;
+  actualGas: number;
+  gasUsed: number;
+  payload: ContractEventPayload;
+};
+
+// TODO: Try replacing with https://medium.com/coinmonks/top-10-polygon-apis-6187fc965851
+export const listen = async (contract: Contract, eventName: string) => {
+  console.log(`Listening for ${eventName} events...`);
+  contract.on(eventName, (...args) => {
+    // LOGGER.info(`Received event: ${eventName}`);
+    const userOpEvent: UserOperationEvent = {
+      userOpHash: args[0],
+      sender: args[1],
+      paymaster: args[2],
+      nonce: args[3],
+      success: args[4],
+      actualGas: args[5],
+      gasUsed: args[6],
+      payload: args[7],
+    };
+    // Send to database
+  });
+};
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { ethers } from "ethers";
+import abiJson from "../abi.json";
+import process from "process";
+import { listen } from "./eventListener";
+
+// Handle interrupt signal
+process.on("SIGINT", () => {
+  console.log("Exiting...");
+  process.exit(0);
+});
+
+// TODO: Setup a logger with multiple levels
+// Maybe winston?
+
+const RPC_URL = "https://polygon-rpc.com";
+const ABI = abiJson.abi;
+const ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
+
+const provider = new ethers.JsonRpcProvider(RPC_URL);
+const contract = new ethers.Contract(ADDRESS, ABI, provider);
+
+listen(contract, "UserOperationEvent");
