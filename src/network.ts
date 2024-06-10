35f75f5a1f815c15bad551874634b1c5b78330cf
Atharva <atharvakn@gmail.com>
2024-06-10 14:19:45 +0530

    feat: retry mechanism

--- src/network.ts
+++ src/network.ts
@@ -2,7 +2,21 @@ import { ethers } from 'ethers'
 
 export async function getSender(txHash: string, provider: ethers.Provider) {
   // TODO: Add a retry mechanism
-  const tx = await provider.getTransaction(txHash)
+  let retry = 3
+  let tx: ethers.TransactionResponse | null = null
+  while (retry > 0) {
+    try {
+      tx = await provider.getTransaction(txHash)
+      if (!tx) {
+        throw new Error(`Transaction not found ${txHash}`)
+      }
+      return tx.from
+    } catch (err) {
+      // Wait 3 seconds
+      await new Promise((resolve) => setTimeout(resolve, 3000))
+      retry--
+    }
+  }
   if (!tx) {
     throw new Error(`Transaction not found ${txHash}`)
   }
